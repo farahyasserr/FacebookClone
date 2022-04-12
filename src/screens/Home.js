e2a499eaeebb6a6468200b9e0e37bf2811f0a4a5
farahyasserr <farah.dawoudd@gmail.com>
2022-04-12 10:56:07 +0200

    initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
