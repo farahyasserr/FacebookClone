dcddb2cfe2b854ef06b01e22161c57838b3fe76f
farahyasserr <farah.dawoudd@gmail.com>
2022-04-12 22:35:03 +0200

    resize renamed

--- src/common/Resize.js
+++ src/common/Resize.js
@@ -1,6 +1,6 @@
 import {Dimensions} from 'react-native';
 const screenWidth = Dimensions.get('window').width;
 
-export default getAdjustedFontSize = size => {
+export default Resize = size => {
   return (parseInt(size) * screenWidth) / 500;
 };
