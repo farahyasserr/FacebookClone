af5fd2583103c0dac7f95a2683e84daa21bab954
farahyasserr <farah.dawoudd@gmail.com>
2022-04-13 15:50:18 +0200

    dummy data updated with image and date

--- src/data/DummyTimelineData.js
+++ src/data/DummyTimelineData.js
@@ -5,9 +5,11 @@ const data = [
     message:
       'This is my first post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
     status_type: 'created_note',
+    image: 'https://picsum.photos/200/300?random=1',
     from: {
       name: 'Farah Yasser',
       icon: 'https://picsum.photos/200',
+      date: '13 Apr 2020',
     },
   },
   {
@@ -16,9 +18,11 @@ const data = [
     message:
       'This is my first post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
     status_type: 'created_note',
+    image: null,
     from: {
       name: 'Ahmed Zaki',
       icon: 'https://picsum.photos/200/300?random=2',
+      date: '21 March 2022',
     },
   },
   {
@@ -27,9 +31,37 @@ const data = [
     message:
       'This is my khs sojsnsojs sl post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
     status_type: 'created_note',
+    image: 'https://picsum.photos/200',
     from: {
       name: 'Menna Mokhtar',
       icon: 'https://picsum.photos/200/300?random=1',
+      date: '20 Jan 2021',
+    },
+  },
+  {
+    id: 4,
+    is_popular: true,
+    message:
+      'This is my khs sojsnsojs sl post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
+    status_type: 'created_note',
+    image: 'https://picsum.photos/200/300?random=1',
+    from: {
+      name: 'Menna Mokhtar',
+      icon: 'https://picsum.photos/200/300?random=1',
+      date: '20 Jan 2021',
+    },
+  },
+  {
+    id: 5,
+    is_popular: true,
+    message:
+      'This is my khs sojsnsojs sl post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
+    status_type: 'created_note',
+    image: 'https://picsum.photos/200/300?random=1',
+    from: {
+      name: 'Ahmed Mohamed',
+      icon: 'https://picsum.photos/200/300?random=1',
+      date: '20 Jan 2021',
     },
   },
 ];
---
+++ src/data/MyAccountDummyData.js
@@ -0,0 +1,6 @@
+const userData = {
+  name: 'Farah Yasser',
+  image: 'https://picsum.photos/200/300?random=1',
+};
+
+export default userData;
