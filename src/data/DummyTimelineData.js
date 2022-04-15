4041029f3a29ede7a17c725a8cb589a241e3ffb6
farahyasserr <farah.dawoudd@gmail.com>
2022-04-15 03:56:32 +0200

    Story Indicator blue circle around profile picture created

--- src/data/DummyTimelineData.js
+++ src/data/DummyTimelineData.js
@@ -1,59 +1,63 @@
 const data = [
   {
     id: 1,
-    is_popular: true,
+    is_public: true,
     message:
       'This is my first post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
     status_type: 'created_note',
-    image: 'https://picsum.photos/200/300?random=1',
+    image: 'https://picsum.photos/id/1014/400/400',
     from: {
       name: 'Farah Yasser',
       icon: 'https://picsum.photos/200',
-      date: '13 Apr 2020',
+      date: '13m',
+      has_story: false,
     },
   },
   {
     id: 2,
-    is_popular: false,
+    is_public: false,
     message:
       'This is my first post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
     status_type: 'created_note',
     image: null,
     from: {
       name: 'Ahmed Zaki',
-      icon: 'https://picsum.photos/200/300?random=2',
+      icon: 'https://picsum.photos/id/1005/400/400',
       date: '21 March 2022',
+      has_story: false,
     },
   },
   {
     id: 3,
-    is_popular: true,
+    is_public: true,
     message:
       'This is my khs sojsnsojs sl post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
     status_type: 'created_note',
-    image: 'https://picsum.photos/200',
+    image: 'https://picsum.photos/id/1010/400/400',
     from: {
       name: 'Menna Mokhtar',
-      icon: 'https://picsum.photos/200/300?random=1',
+      icon: 'https://picsum.photos/id/101/400/400',
       date: '20 Jan 2021',
+      has_story: false,
     },
   },
   {
     id: 4,
-    is_popular: true,
+    is_public: true,
     message:
       'This is my khs sojsnsojs sl post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
     status_type: 'created_note',
-    image: 'https://picsum.photos/200/300?random=1',
+    image: 'https://picsum.photos/id/1015/400/400',
     from: {
       name: 'Menna Mokhtar',
       icon: 'https://picsum.photos/200/300?random=1',
       date: '20 Jan 2021',
+      has_story: false,
     },
   },
   {
     id: 5,
-    is_popular: true,
+    is_public: true,
     message:
       'This is my khs sojsnsojs sl post, wanted to thank my kjsoj jsw and my parents akns ksns skjss ks',
     status_type: 'created_note',
@@ -62,6 +66,7 @@ const data = [
       name: 'Ahmed Mohamed',
       icon: 'https://picsum.photos/200/300?random=1',
       date: '20 Jan 2021',
+      has_story: true,
     },
   },
 ];
--- src/screens/home/post/ProfileIcon.js
+++ src/screens/home/post/ProfileIcon.js
@@ -1,29 +1,49 @@
 import React from 'react';
-import {StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {StyleSheet, Image, TouchableOpacity, View} from 'react-native';
+import colors from '../../../common/Colors';
 import Resize from '../../../common/Resize';
 
-const ProfileIcon = ({uri}) => {
+const ProfileIcon = ({uri, user}) => {
   return (
-    <TouchableOpacity style={styles.imageContainer}>
-      <Image
-        style={styles.icon}
-        source={{
-          uri: uri,
-        }}
-      />
-    </TouchableOpacity>
+    <View style={user?.has_story && styles.storyCircle}>
+      <View style={user?.has_story && styles.whiteArea}>
+        <TouchableOpacity style={styles.imageContainer}>
+          <Image
+            style={styles.icon}
+            source={{
+              uri: uri,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   icon: {
     height: Resize(48),
-    width: Resize(48)
+    width: Resize(48),
   },
-  imageContainer:{
-      borderRadius: Resize(48)/2,
-      overflow: 'hidden',
-      alignSelf: 'flex-start',
+  imageContainer: {
+    borderRadius: Resize(48) / 2,
+    overflow: 'hidden',
+    alignSelf: 'center',
+  },
+  storyCircle: {
+    height: Resize(56),
+    width: Resize(56),
+    borderRadius: Resize(56) / 2,
+    backgroundColor: colors.blue,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  whiteArea:{
+    height: Resize(50),
+    width: Resize(50),
+    borderRadius: Resize(50) / 2,
+    backgroundColor: colors.white,
+    justifyContent: 'center',
   }
 });
 export default ProfileIcon;
