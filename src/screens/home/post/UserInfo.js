c3a2ade931dc43aca4817462d76687e771d8a99b
farahyasserr <farah.dawoudd@gmail.com>
2022-04-15 06:05:16 +0200

    user info section that includes date, scope and settings of post

---
+++ src/screens/home/post/UserInfo.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import fonts from '../../../assets/Fonts';
+import Resize from '../../../common/Resize';
+import ProfileIcon from './ProfileIcon';
+import Icon3 from 'react-native-vector-icons/FontAwesome5';
+import Icon4 from 'react-native-vector-icons/Ionicons';
+import PostSettingsComponent from './PostSettingsComponent';
+
+const UserInfo = ({user, post}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.firstSubContainer}>
+        <ProfileIcon uri={user.icon} user={user} />
+        <View style={styles.horizontalView}>
+          <View style={styles.nameAndSettingsContainer}>
+            <Text style={styles.nameText}>{user.name}</Text>
+            <PostSettingsComponent />
+          </View>
+          <View style={styles.postInfoContainer}>
+            <Text style={styles.dateText}>{user.date} . </Text>
+            {post.is_public === true ? (
+              <Icon4 name="ios-earth" size={12} />
+            ) : (
+              <Icon3
+                name="user-friends"
+                size={10}
+                style={{alignSelf: 'center'}}
+              />
+            )}
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: Resize(10),
+  },
+  nameText: {
+    marginStart: Resize(10),
+    fontFamily: fonts.montserrat_medium,
+  },
+  firstSubContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+  },
+  dateText: {
+    fontFamily: fonts.montserrat_regular,
+    fontSize: Resize(14),
+    marginStart: Resize(10),
+  },
+  postInfoContainer: {
+    flexDirection: 'row',
+  },
+  nameAndSettingsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  horizontalView: {
+    flex: 1,
+  },
+});
+export default UserInfo;
